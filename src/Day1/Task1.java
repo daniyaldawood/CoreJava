4bcc387d5cbc63ea578aa13cf4aafe5e8b14cbe6
Daniyal Dawood <daniyal.dawood@capgemini.com>
2021-10-11 17:10:09 +0530

    Task1

---
+++ src/Day1/Task1.java
@@ -0,0 +1,11 @@
+package Day1;
+
+public class Task1 {
+	
+	private int x,y;
+	
+	public Task1(int x, int y) {
+	System.out.println("Constructor with two parameters");
+	}
+}
+
---
+++ src/Day1/Task2.java
@@ -0,0 +1,11 @@
+package Day1;
+
+public class Task2 {
+	
+	private int x,y;
+
+	public String getDetails()
+	{
+	   return "Point ("+x+","+y+")";
+	}
+}
